9aa3b7d5dac11adba6cb72b1667621e5b11863c2
Enki Garrigues <enki.garrigues@gfi.fr>
2020-04-07 18:22:34 +0200

    Begin add database

---
+++ Data/DataAccess/BillingDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Common.EntityModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class BillingDbContext : DbContext
+    {
+        public BillingDbContext()
+        {
+        }
+
+        public BillingDbContext(DbContextOptions<BillingDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Facture> Factures { get; set; }
+        public virtual DbSet<Devis> Devis { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
